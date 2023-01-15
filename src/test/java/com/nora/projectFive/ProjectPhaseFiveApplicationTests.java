41c3d547ccd142d712772e469a0ce7a772a6b6c1
nora alobaidi <101515331+noraalobaidi@users.noreply.github.com>
2023-01-15 08:56:46 +0300

    CREATED springboot project for the backend

---
+++ src/test/java/com/nora/projectFive/ProjectPhaseFiveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nora.projectFive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectPhaseFiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
